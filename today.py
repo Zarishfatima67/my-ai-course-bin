37883861fa5e5308f19874ed9bb3b22ad547e80c
Zarish fatima <zariijutt493@gmail.com>
2026-05-03 17:48:02 +0500

    PRACTICE

---
+++ today.py
@@ -0,0 +1,53 @@
+  
+
+print("hello world")
+
+Age=int(input('please enter your Age:'))
+Height=float(input("enter value here"))
+print("Age",Age)
+print("Height",Height)
+print(type(Age))
+print(type(Height))
+
+sum=Age+Height
+print("sum")
+print ('sum: ', Age + Height) 
+print(type(sum))
+sub=Age-Height
+print(sub)
+print ('Subtraction: ', Age - Height) 
+print(type(sub))
+multiple=Age*Height
+print ('multiplication: ', multiple) 
+print(type(multiple))
+Div=Age/Height
+print ('Divison: ', Age /Height) 
+print(type(Div))
+floatDivisoion=Age//Height
+print ('Floor Division: ',floatDivisoion)
+print(type(floatDivisoion))
+mod=Age%Height
+print ('Modulo: ',mod )
+print(type(mod))
+print(' Age == Height =', Age == Height)
+print('Age != Height =', Age!= Height)
+print('Age> Height =', Age> Height)
+print('Age < Height=', Age< Height)
+print('Age >= Height=', Age >= Height)
+print('Age <= Height =', Age<= Height)
+Age=Age+20
+Age+=20
+print(Age)
+print ("Age += 20 : ", Age)
+Age -= 20
+print ("Age -= 20 : ", Age)
+Age *= 20
+print ("Age *= 20 : ", Age)
+Age/= 20
+print ("Age /= 20 : ",Age)
+Age %= 20
+print ("Age%= 20 : ", Age)
+Age **= 20
+print ("Age**= 20 : ", Age)
+Age //= 20
+print ("Age //= 20 : ", Age)
