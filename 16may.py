cd3ea354fc89c7136aa5c241208fc97df2f6fbea
Zarish fatima <zariijutt493@gmail.com>
2026-05-16 17:24:47 +0500

    16may

---
+++ 16may.py
@@ -0,0 +1,17 @@
+
+# MY Practice code
+lhrinfo="lahore is capital of punjab and islamabad is captial of pakistan"
+print(lhrinfo)
+print(type(lhrinfo))
+
+print(lhrinfo.upper())
+print(lhrinfo.lower())
+print(lhrinfo.capitalize())
+print(lhrinfo.swapcase())
+print(lhrinfo.title())
+print(lhrinfo[0:6:1])
+print(lhrinfo[9:17:1])
+print(lhrinfo[20:28:1])
+print(len(lhrinfo))
+
+
